8ae87c369957aab79b991cff94db9758f4bb3efe
Briteless <chestreb@gmail.com>
2018-06-28 14:26:34 +0100

    Age Calc
    Fixed JS for age calculation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-document.getElementById('age').innerHTML = 'Full-Stack Developer, Lisbon, Age 23';
+var today = new Date;
+var year = today.getFullYear();
+var age = year - 1994
+
+document.getElementById('age').innerHTML = 'Full-Stack Developer, Lisbon, Age ' + age;
