02a52d03dbb64690f09d4c58fde0496740a666eb
Briteless <chestreb@gmail.com>
2018-06-28 14:46:49 +0100

    Fix of the fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var today = new Date;
 var year = today.getFullYear();
-var age = year - 1980
+var age = year - 1994
 
 document.getElementById('age').innerHTML = 'Full-Stack Developer, Lisbon, Age ' + age;
