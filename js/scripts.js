4fd2e1eb4a86a7b29bb9bc660a7239158ede636c
Pedro Brito <chestreb@gmail.com>
2018-06-28 14:42:28 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var today = new Date;
 var year = today.getFullYear();
-var age = year - 1994
+var age = year - 1980
 
 document.getElementById('age').innerHTML = 'Full-Stack Developer, Lisbon, Age ' + age;
